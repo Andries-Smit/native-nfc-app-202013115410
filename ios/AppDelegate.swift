a5baed3a8c414328a8261481c0a5d67828c63d74
Andries Smit <Andries.Smit@mendix.com>
2019-11-30 21:23:00 +0100

    Add iOS localhost template

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -10,7 +10,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     guard let url = Bundle.main.object(forInfoDictionaryKey: "Runtime url") as? String, let runTimeUrl = AppUrl.forRuntime(url: url) else {
       fatalError("Missing the 'Runtime url' configuration within the Info.plist file")
     }
-    guard let bundleUrl = ReactNative.instance.getJSBundleFile() else {
+    // guard let bundleUrl = ReactNative.instance.getJSBundleFile() else {
+    guard let bundleUrl = AppUrl.forBundle(url: "http://10.201.200.160:8080", remoteDebuggingPackagerPort: 8083, isDebuggingRemotely: true, isDevModeEnabled: false) else {
       fatalError("Could not properly load JS bundle file")
     }
 
